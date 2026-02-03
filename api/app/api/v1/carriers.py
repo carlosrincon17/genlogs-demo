803b4c759ba6a7031ad8db687de6a4f8c1e37e36
Carlos Rincon <carlosjrinconavila@gmail.com>
2026-02-03 00:37:01 -0500

    feat: added deployment stages

--- api/app/api/v1/carriers.py
+++ api/app/api/v1/carriers.py
@@ -7,6 +7,6 @@ from app.models.carrier_model import Carrier, CarrierSearchRequest
 
 router = APIRouter()
 
-@router.get("/find", response_model=List[Carrier])
+@router.get("/search", response_model=List[Carrier])
 def search_carriers(request: CarrierSearchRequest = Depends()):
     return CarrierController.search_carriers(request.from_city, request.to_city)
